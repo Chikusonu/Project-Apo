228d3d7895ad6b8c91caa14e1e982172fd797aba
Chikusonu <druva186@gmail.com>
2022-06-05 16:13:04 +0530

    1 file modified

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -11,6 +11,17 @@ export default function Restaurant() {
             <span className="card-author subtle" style={{ color: 'black' }}>
               breakfastbreakfast
             </span>
+            <h1 className="card-title">maggi</h1>
+            <div classNames="card-descriptio subtl">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
+              reprehenderit optio amet ab temporibus asperiores quasi
+              cupiditate. Voluptatum ducimus voluptates voluptas?
+            </div>
+            <div className="card-read">
+READ 
+            </div>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPYV_Y8uOeIEMVJnDLYCbgUJwONFFCVE-l4TNYS3TtIHFyfwbythwyl0s&s=10" alt="image" className="card-media" />
           </div>
         </div>
       </div>
