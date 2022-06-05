f98b6997a227be36d5e330e184c1034cc0d008de
Chikusonu <druva186@gmail.com>
2022-06-05 13:31:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import Restaurant from './component/Basics/Restaurant';
+
+
+export default function App() {
+  return (
+    <div>
+      <Restaurant />
+    </div>
+  );
+}
